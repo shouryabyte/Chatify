37443d26abf25b560dae60790d8edbb18a906f22
Shourya <2306063@kiit.ac.in>
2025-10-18 21:10:03 +0530

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,8 +32,11 @@ export const signup = async (req, res) => {
         })
 
         if(newUser){
-            generateToken(newUser._id,res)
-            await newUser.save()
+            // generateToken(newUser._id,res)
+            // await newUser.save()
+            //after coderabbit
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
             res.status(201).json({
                 _id:newUser._id,
                 fullName:newUser.fullName,
